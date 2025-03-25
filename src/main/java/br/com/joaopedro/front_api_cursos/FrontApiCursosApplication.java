4ca1d6d87422a004ede3b0749b05df311e1d32d8
João Pedro <joaope.espindola2005@gmail.com>
2025-03-25 13:50:16 -0300

    Thymeleaf

---
+++ src/main/java/br/com/joaopedro/front_api_cursos/FrontApiCursosApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaopedro.front_api_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontApiCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontApiCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaopedro/front_api_cursos/FrontApiCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopedro.front_api_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApiCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
